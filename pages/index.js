1b918818b7e70c663d5c1463dcb8004d4dfe06b0
Mustafa Toy <mustafatoy678@gmail.com>
2022-01-23 00:53:20 +0300

    initial version v1

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Mustafa Toy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Mustafa Toy" />
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
